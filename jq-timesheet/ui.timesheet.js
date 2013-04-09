dd60a2a17df1b780d329acb83232c9d48265a98c
Rohit Mitra <rohitmitra5752@gmail.com>
2013-04-09 17:25:31 +0530

    Changes to be committed:
    	modified:   ui.timesheet.js

--- jq-timesheet/ui.timesheet.js
+++ jq-timesheet/ui.timesheet.js
@@ -1,21 +1,3 @@
-/*$(function(){
-	var rows = 7;
-	var columns = 5;
-	var array=$("<div id='timesheetDiv'></div>");
-	for(i=1;i<=rows;i++)
-	{
-		var row=$("<ol class='timeSheetRow'></ol>");
-		for(j=1;j<=columns;j++)
-		{
-			column=$("<li></li>");
-			$(row).append(column);
-		}
-		$(array).append(row);
-	}
-	$("#test").html(array);
-	$(".timeSheetRow").selectable();
-});*/
-
 (function ($) {
 	$.fn.timeSheet = function(options) {
 		var settings = $.extend({
